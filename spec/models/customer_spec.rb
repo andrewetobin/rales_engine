52d27feb36512b76c19faffc1baa44b8720fb032
andrewetobin <andrewetobin@gmail.com>
2018-10-02 14:37:06 -0600

    customer validations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,8 @@ describe Customer, type: :model do
     it {should have_many(:transactions).through(:invoices)}
     it {should have_many(:merchants).through(:invoices)}
   end
+  describe "validations" do
+    it {should validate_presence_of :first_name}
+    it {should validate_presence_of :last_name}
+  end
 end
