dfca155e4a314c46654636d499a3a50dc7c710ef
andrewetobin <andrewetobin@gmail.com>
2018-10-02 14:34:49 -0600

    customer relationships

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Customer, type: :model do
+  describe "relationships" do
+    it {should have_many :invoices}
+    it {should have_many(:transactions).through(:invoices)}
+    it {should have_many(:merchants).through(:invoices)}
+  end
 end
