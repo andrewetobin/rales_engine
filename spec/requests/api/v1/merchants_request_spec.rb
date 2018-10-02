7ced495e12c1c6b4bcdd1d915c515cb6140d7df5
andrewetobin <andrewetobin@gmail.com>
2018-10-01 19:28:31 -0600

    first test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,3 +1,12 @@
 require 'rails_helper'
 
-describe 
+describe 'Merchants API' do
+  it "sends a list of merchants" do
+    create_list(:merchant, 3)
+
+    get 'api/v1/merchants'
+
+    expect(response).to be_successful
+
+  end
+end
