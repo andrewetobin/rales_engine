aaa0377f046b4e7b40e665cfc38b9ef0c9a237fb
andrewetobin <andrewetobin@gmail.com>
2018-10-02 18:04:51 -0600

    adds spec and controllers to most revenue BI

---
+++ app/controllers/api/v1/merchants/merchant_revenue_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Merchants::MerchantRevenueController < ApplicationController
+  def index
+    params[:quantity] = 5 unless params[:quantity]
+    render json: Merchant.most_revenue(params[:quantity].to_i)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/most_revenue', to: 'merchant_revenue#index'
+      end
+
+
+
+
+
       resources :merchants, only: [:index, :show]
     end
   end
